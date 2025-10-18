4b7cfc3a7adadd9b4257b33900190c994aa59ec4
zhichao-e3a <zhichao.lin@e3ahealth.com>
2025-10-18 23:24:38 +0800

    Removed data pipeline pages

--- dashboard_app.py
+++ dashboard_app.py
@@ -1,3 +1,5 @@
+from cache import get_mongo_connector
+
 from pathlib import Path
 
 import streamlit as st
@@ -7,30 +9,27 @@ if "logged_in" not in st.session_state:
 
 st.session_state.logged_in = True
 
+_ = get_mongo_connector()
+
 ROOT = Path(__file__).resolve().parent
 
 LOGIN               = ROOT / "pages" / "login.py"
 PATIENT_ANALYTICS   = ROOT / "pages" / "patient_analytics.py"
 UPDATE_MODEL_DATA   = ROOT / "pages" / "run_update.py"
 RUN_PIPELINE        = ROOT / "pages" / "run_pipeline.py"
-DATA_MONITORING     = ROOT / "pages" / "etl_monitoring.py"
 
 login_page             = st.Page(LOGIN, title="Login")
 patient_analytics_page = st.Page(PATIENT_ANALYTICS, title="Patient Analytics", icon=":material/person:")
 update_model_data_page = st.Page(UPDATE_MODEL_DATA, title="Update Model Data", icon=":material/database_upload:")
 run_pipeline_page      = st.Page(RUN_PIPELINE, title="Run Data Pipeline", icon=":material/automation:")
-data_monitoring_page   = st.Page(DATA_MONITORING, title="Data Monitoring", icon=":material/monitor_heart:")
 
 if st.session_state.logged_in:
     pg = st.navigation(
-        {
-            "ETL" : [
-                patient_analytics_page,
-                update_model_data_page,
-                run_pipeline_page,
-                # data_monitoring_page
-            ]
-        },
+        [
+            patient_analytics_page,
+            # update_model_data_page,
+            # run_pipeline_page
+        ],
         position="sidebar"
     )
 else:
