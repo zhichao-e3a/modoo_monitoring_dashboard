6ee5e3d0df4b54c5069df9fa7e73014d3c795ce4
zhichao-e3a <zhichao.lin@e3ahealth.com>
2025-11-24 18:22:21 +0800

    Clean up files

--- main_app.py
+++ main_app.py
@@ -15,20 +15,14 @@ ROOT = Path(__file__).resolve().parent
 
 LOGIN               = ROOT / "pages" / "login.py"
 PATIENT_ANALYTICS   = ROOT / "pages" / "patient_analytics.py"
-UPDATE_MODEL_DATA   = ROOT / "pages" / "run_update.py"
-RUN_PIPELINE        = ROOT / "pages" / "run_pipeline.py"
 
 login_page             = st.Page(LOGIN, title="Login")
 patient_analytics_page = st.Page(PATIENT_ANALYTICS, title="Patient Analytics", icon=":material/person:")
-update_model_data_page = st.Page(UPDATE_MODEL_DATA, title="Update Model Data", icon=":material/database_upload:")
-run_pipeline_page      = st.Page(RUN_PIPELINE, title="Run Data Pipeline", icon=":material/automation:")
 
 if st.session_state.logged_in:
     pg = st.navigation(
         [
-            patient_analytics_page,
-            # update_model_data_page,
-            # run_pipeline_page
+            patient_analytics_page
         ],
         position="sidebar"
     )
