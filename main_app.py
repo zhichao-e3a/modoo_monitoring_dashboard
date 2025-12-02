5a5a89b0363aec2b2cd163872b4679990a4c1f50
zhichao-e3a <zhichao.lin@e3ahealth.com>
2025-12-02 16:03:25 +0800

    Removed old pages, added new EDA page

--- main_app.py
+++ main_app.py
@@ -1,5 +1,3 @@
-from cache import get_mongo_connector
-
 from pathlib import Path
 
 import streamlit as st
@@ -9,20 +7,21 @@ if "logged_in" not in st.session_state:
 
 st.session_state.logged_in = True
 
-_ = get_mongo_connector()
-
 ROOT = Path(__file__).resolve().parent
 
 LOGIN               = ROOT / "pages" / "login.py"
 PATIENT_ANALYTICS   = ROOT / "pages" / "patient_analytics.py"
+MEASUREMENTS_EDA    = ROOT / "pages" / "eda.py"
 
 login_page             = st.Page(LOGIN, title="Login")
 patient_analytics_page = st.Page(PATIENT_ANALYTICS, title="Patient Analytics", icon=":material/person:")
+measurements_eda_page  = st.Page(MEASUREMENTS_EDA, title="Data Analysis", icon=":material/frame_inspect:")
 
 if st.session_state.logged_in:
     pg = st.navigation(
         [
-            patient_analytics_page
+            patient_analytics_page,
+            measurements_eda_page
         ],
         position="sidebar"
     )
