75e73ed842d084fdd77d10137b088500eb46e224
zhichao-e3a <zhichao.lin@e3ahealth.com>
2025-12-02 16:04:53 +0800

    Removed DB connector, added pymongo

--- cache.py
+++ cache.py
@@ -1,15 +1,23 @@
-from database.MongoDBConnector import MongoDBConnector
+from config.configs import REMOTE_MONGO_CONFIG
+from pymongo import MongoClient
 
 import streamlit as st
-import asyncio
-
-@st.cache_resource
-def get_mongo_connector():
-    return MongoDBConnector(mode="remote")
 
 @st.cache_data(show_spinner=True, ttl=60)
-def get_data(coll_name: str):
+def get_data(coll_name: str, projection: dict=None, limit: int=None):
 
-    mongo = get_mongo_connector()
+    uri     = REMOTE_MONGO_CONFIG['DB_HOST']
+    db_name = REMOTE_MONGO_CONFIG["DB_NAME"]
+    client  = MongoClient(uri)
+    coll    = client[db_name][coll_name]
 
-    return asyncio.run(mongo.get_all_documents(coll_name=coll_name))
+    docs = coll.find({}, projection or {"_id": 0})
+
+    if limit:
+        docs = docs.limit(limit)
+
+    docs_list = list(docs)
+
+    client.close()
+
+    return docs_list
